7ff1d0dd41d8bc9456ac0daaff583a016e866dd2
Yasmur <phememomon@.com>
2020-10-01 00:19:33 +0300

    Updates

---
+++ precache-manifest.08d8094da07f7bba8ac823c9d7055dd6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af4397c5450d33deeb5035c2a2bc2e5",
+    "url": "/Yasmur/App.git/index.html"
+  },
+  {
+    "revision": "ad6fbc06c06535f805b6",
+    "url": "/Yasmur/App.git/static/css/2.13d64717.chunk.css"
+  },
+  {
+    "revision": "815fffd6df9f83de583b",
+    "url": "/Yasmur/App.git/static/css/main.bcbfd914.chunk.css"
+  },
+  {
+    "revision": "ad6fbc06c06535f805b6",
+    "url": "/Yasmur/App.git/static/js/2.decdbf2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yasmur/App.git/static/js/2.decdbf2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815fffd6df9f83de583b",
+    "url": "/Yasmur/App.git/static/js/main.42294880.chunk.js"
+  },
+  {
+    "revision": "1423bdde717e5f3ff670",
+    "url": "/Yasmur/App.git/static/js/runtime-main.8e3130c5.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/Yasmur/App.git/static/media/GC 2.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/Yasmur/App.git/static/media/ITN_mix 1.2f311a05.svg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/Yasmur/App.git/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/Yasmur/App.git/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "b236af76d3e12b9f2dc0a2d673375d86",
+    "url": "/Yasmur/App.git/static/media/hero-bg.b236af76.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/Yasmur/App.git/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/Yasmur/App.git/static/media/shid_emozhl_logo_white 1.341a487c.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/Yasmur/App.git/static/media/sity_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/Yasmur/App.git/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/Yasmur/App.git/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/Yasmur/App.git/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/Yasmur/App.git/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/Yasmur/App.git/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/Yasmur/App.git/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/Yasmur/App.git/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
